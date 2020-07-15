2116e28ce364f65935bfd6d0c628e04eb2d9f72b
Arturo Chen <art.can.fly@gmail.com>
2020-07-15 10:26:00 -0400

    initial commit

--- server.js
+++ server.js
@@ -7,15 +7,6 @@ const { flights } = require('./test-data/flightSeating');
 
 const PORT = process.env.PORT || 8000;
 
-const handleFlight = (req, res) => {
-    const { flightNumber } = req.params;
-    // get all flight numbers
-    const allFlights = Object.keys(flights);
-    // is flightNumber in the array?
-    console.log('REAL FLIGHT: ', allFlights.includes(flightNumber))
-}
-
-{name: 'Fred'}
 express()
     .use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
@@ -28,6 +19,5 @@ express()
     .use(express.urlencoded({extended: false}))
     
     // endpoints
-    .get('/flights/:flightNumber', handleFlight)
     .use((req, res) => res.send('Not Found'))
     .listen(PORT, () => console.log(`Listening on port ${PORT}`));

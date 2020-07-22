bb96e48b24dc8187a9874f1f4bc0e1f1160bab6f
Arturo Chen <art.can.fly@gmail.com>
2020-07-22 00:08:15 -0400

    indexRouter moved lower

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/', indexRouter)
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
-	)
-	next()
-})
+		)
+		next()
+	})
 
+app.use('/', indexRouter)
 app.set('view engine', 'ejs')
 app.listen(process.env.PORT || 8000)
